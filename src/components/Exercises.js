654130c378d4df36a8cfa826155128e754406529
Thongzxje <thonghoangdanh@gmail.com>
2022-12-23 16:18:06 +0200

    some fix

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,11 +1,10 @@
 import { useEffect, useState } from "react";
 import { Pagination } from "@mui/material";
 import { Box, Stack, Typography } from "@mui/material/";
-import React from 'react';
+import React from "react";
 
 import { exerciseOptions, fetchData } from "../utils/fetchData";
 import ExerciseCard from "./ExerciseCard";
-import BodyPart from "./BodyPart";
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -13,47 +12,55 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
 
   const indexOfLastExercise = currentPage * exercisesPerPage;
   const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
-  const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise);
+  const currentExercises = exercises.slice(
+    indexOfFirstExercise,
+    indexOfLastExercise
+  );
 
   const paginate = (e, value) => {
     setCurrentPage(value);
-    window.scrollTo({ top: 1800, behavior: 'smooth'});
-  }
+    window.scrollTo({ top: 1800, behavior: "smooth" });
+  };
 
   useEffect(() => {
     const fetchExercisesData = async () => {
       let exercisesData = [];
-      if (bodyPart === 'all') {
-        exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
-
+      if (bodyPart === "all") {
+        exercisesData = await fetchData(
+          "https://exercisedb.p.rapidapi.com/exercises",
+          exerciseOptions
+        );
       } else {
-        exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`, exerciseOptions);
+        exercisesData = await fetchData(
+          `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`,
+          exerciseOptions
+        );
       }
 
       setExercises(exercisesData);
     };
 
     fetchExercisesData();
-  }, [bodyPart])
+  }, [bodyPart]);
 
   return (
-    <Box
-      id="exercises"
-      sx={{ mt: {lg:'110px'}}}
-      mt="50px"
-      p="20px"
-    >
+    <Box id="exercises" sx={{ mt: { lg: "110px" } }} mt="50px" p="20px">
       <Typography variant="h3" mb="46px" color="#395144">
         Showing results
       </Typography>
-      <Stack direction="row" sx={{gap: {lg: '110px', xs: '50px'}}} flexWrap="wrap" justifyContent="center">
+      <Stack
+        direction="row"
+        sx={{ gap: { lg: "110px", xs: "50px" } }}
+        flexWrap="wrap"
+        justifyContent="center"
+      >
         {currentExercises.map((exercise, index) => (
-          <ExerciseCard key={index} exercise={exercise}/>
+          <ExerciseCard key={index} exercise={exercise} />
         ))}
       </Stack>
       <Stack mt="100px" alignItems="center">
         {exercises.length > 9 && (
-          <Pagination 
+          <Pagination
             color="standard"
             shape="rounded"
             defaultPage={1}
@@ -65,7 +72,7 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
         )}
       </Stack>
     </Box>
-  )
-}
+  );
+};
 
-export default Exercises
+export default Exercises;
--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,41 +1,54 @@
-import React from 'react';
-import { Box, Stack, Typography, Button } from '@mui/material';
-import HeroBannerImage from '../assets/images/banner.png'
+import React from "react";
+import { Box, Typography, Button } from "@mui/material";
+import HeroBannerImage from "../assets/images/banner.png";
 
 const HeroBanner = () => {
   return (
-    <Box sx={{
-      mt: { lg: '212px', xs: '70px'},
-      ml: { sm: '50px'}
-    }} position="relative" p="20px">
+    <Box
+      sx={{
+        mt: { lg: "212px", xs: "70px" },
+        ml: { sm: "50px" },
+      }}
+      position="relative"
+      p="20px"
+    >
       <Typography color="#61764B" fontWeight="600" fontSize="26px">
         Fitness Club
       </Typography>
-      <Typography fontWeight="700"
-      color="#9ba17b"
-      sx={{fontSize: {lg: '44px', xs: '40px'}}}
-      mb="23px" mt="30px"
+      <Typography
+        fontWeight="700"
+        color="#9ba17b"
+        sx={{ fontSize: { lg: "44px", xs: "40px" } }}
+        mb="23px"
+        mt="30px"
       >
         Sweat, Smile <br /> and Repeat
       </Typography>
       <Typography fontSize="22px" lineHeight="35px" mb={4}>
         Check out the most effective exercises
       </Typography>
-      <Button variant="contained" color="error" href="#exercises" sx={{backgroundColor: '#61764B', padding: '10px'}}>Explore Exercises</Button>
+      <Button
+        variant="contained"
+        color="error"
+        href="#exercises"
+        sx={{ backgroundColor: "#61764B", padding: "10px" }}
+      >
+        Explore Exercises
+      </Button>
       <Typography
         fontWeight={600}
         color="#cfb997"
         sx={{
           opacity: 0.3,
-          display: { lg: 'block', xs: 'none'}, 
+          display: { lg: "block", xs: "none" },
         }}
         fontSize="200px"
       >
         Exercises
       </Typography>
-      <img src={HeroBannerImage} alt="banner" className='hero-banner-img' />
+      <img src={HeroBannerImage} alt="banner" className="hero-banner-img" />
     </Box>
-  )
-}
+  );
+};
 
-export default HeroBanner
+export default HeroBanner;
